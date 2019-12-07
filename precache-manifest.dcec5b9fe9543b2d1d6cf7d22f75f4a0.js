82497f2e1a0ff6d75128162fe095eb5a27005fad
somayeh ghasemipour <somayeh.ghasemipour@gmail.com>
2019-12-07 13:36:41 +0330

    Updates

---
+++ precache-manifest.dcec5b9fe9543b2d1d6cf7d22f75f4a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4981dd9e2785013e55482980b00edf03",
+    "url": "/contactManager/index.html"
+  },
+  {
+    "revision": "84db65a5ac82bda63018",
+    "url": "/contactManager/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "3bb660c4fc47ead99b25",
+    "url": "/contactManager/static/css/main.65938976.chunk.css"
+  },
+  {
+    "revision": "84db65a5ac82bda63018",
+    "url": "/contactManager/static/js/2.40279019.chunk.js"
+  },
+  {
+    "revision": "3bb660c4fc47ead99b25",
+    "url": "/contactManager/static/js/main.b765ef45.chunk.js"
+  },
+  {
+    "revision": "ff9c7a2e87278a7ebbd4",
+    "url": "/contactManager/static/js/runtime-main.6835edec.js"
+  }
+]);
